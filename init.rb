a756b39d0c08fad24e993513a6ef8bcfd3715464
Cyril Mougel <cyril.mougel@gmail.com>
2010-03-28 00:01:13 +0100

    starting little sidebar copy of search_sidebar

---
+++ init.rb
@@ -0,0 +1,4 @@
+require 'sidebar'
+require 'searchsubmitimage_sidebar'
+
+SearchsubmitimageSidebar.view_root = File.dirname(__FILE__) + '/views'
---
+++ lib/searchsubmitimage_sidebar.rb
@@ -0,0 +1,6 @@
+class SearchsubmitimageSidebar < Sidebar
+
+  description "Adds basic search sidebar in your Typo blog with submit like image"
+
+  setting :title, 'Search with image submit'
+end
